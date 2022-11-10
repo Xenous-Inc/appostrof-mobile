da8983f77ad533f1f32dbbe7778e95f7242eccd8
x3noku <x3noku@gmail.com>
2022-11-11 00:00:17 +0300

    Add logger commit

---
+++ src/utils/logs.ts
@@ -0,0 +1,26 @@
+import { consoleTransport, logger } from 'xenous-logs';
+
+const config = {
+    transport: consoleTransport,
+    severity: __DEV__ ? 'debug' : 'error',
+    levels: {
+        info: 6,
+        error: 3,
+        // dev levels
+        warn: 2,
+        debug: 1,
+    },
+    transportOptions: {
+        colors: 'ansi',
+    },
+    enabledExtensions: ['ui', 'hooks', 'redux', 'api', 'middlewares', 'utils'] as const,
+    async: true,
+    dateFormat: __DEV__ ? 'time' : 'utc',
+    printLevel: true,
+    printDate: true,
+    enabled: true,
+};
+
+const logs = logger.createLogger<typeof config.levels, typeof config.enabledExtensions[number]>(config);
+
+export default logs;
