5f693500018aec7f6d5a6ad632609a0bd7211d17
terraqbit <terraqbit@gmail.com>
2022-11-17 17:47:06 +0700

    Add and fix fonts commit - add correct SemiBold font - add Regular font

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { useFonts } from 'expo-font';
 export default function App() {
     const [loaded] = useFonts({
         SemiBold: require('../assets/fonts/SemiBold.ttf'),
+        Regular: require('../assets/fonts/Regular.ttf'),
     });
 
     if (!loaded) {
