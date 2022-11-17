d587100c2e2b083f85905b91f34bce125093f6cc
terraqbit <terraqbit@gmail.com>
2022-11-17 16:48:39 +0700

    Add SemiBold and create part of story screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Button from '@components/atoms/button';
-import Center from '@components/atoms/center';
-import Space from '@components/atoms/space';
+import StoryScreen from '@components/organisms/story_screen';
+import { useFonts } from 'expo-font';
 
 export default function App() {
-    return (
-        <Center>
-            <Button title={'Цветная кнопка'} mode={Button.Mode.Contained} />
-            <Space mode={Space.Mode.Horizontal} size={16} />
-            <Button
-                title={'Цветная кастомная кнопка'}
-                color={'coral'}
-                backgroundColor={'pink'}
-                mode={Button.Mode.Contained}
-            />
-            <Space mode={Space.Mode.Horizontal} size={16} />
-            <Button title={'Назад'} mode={Button.Mode.Blank} />
-            <Space mode={Space.Mode.Horizontal} size={16} />
-            <Button
-                title={'Кастомный назад'}
-                borderColor={'red'}
-                containerStyle={{ borderWidth: 1 }}
-                backgroundColor={'red'}
-                mode={Button.Mode.Blank}
-            />
-        </Center>
-    );
+    const [loaded] = useFonts({
+        SemiBold: require('../assets/fonts/SemiBold.ttf'),
+    });
+
+    if (!loaded) {
+        return null;
+    }
+    return <StoryScreen />;
 }
