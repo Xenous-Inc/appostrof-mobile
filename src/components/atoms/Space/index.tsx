37bca47b6733fafae51f87084fd487b2f7623229
x3noku <x3noku@gmail.com>
2022-11-25 16:04:46 +0300

    fix-ppst-5/story-screen update components

--- src/components/atoms/Space/index.tsx
+++ src/components/atoms/Space/index.tsx
@@ -9,14 +9,14 @@ enum Mode {
 type TMode = typeof Mode;
 
 interface ISpaceProp {
-    mode: Mode;
+    mode: Mode | Mode[keyof Mode];
 
     size: number;
 }
 
 const Space: React.FC<ISpaceProp> & { Mode: TMode } = ({ mode, size }) => {
-    const width = mode === Mode.Horizontal ? 0 : size;
-    const height = mode === Mode.Vertical ? 0 : size;
+    const width = mode === Mode.Horizontal ? size : 0;
+    const height = mode === Mode.Vertical ? size : 0;
 
     return <View style={{ width, height }} />;
 };
